025cc78e039d465794fd0c9a953dd7ee0d6bbeb6
Mikkelbos <mikkeldollerup@gmail.com>
2024-02-06 12:30:46 +0100

    ,æø

--- discrete_choice.py
+++ discrete_choice.py
@@ -3,8 +3,69 @@ import pandas as pd
 import mestim as M
 from tabulate import tabulate
 from numpy import linalg as la
+from scipy.optimize import minimize
 from numpy import random
+import time
 from scipy.stats import genextreme
+
+def estimation(Qfun, theta0, deriv=0, cov_type ='sandwich', parnames='', output=False): 
+
+    tic = time.perf_counter() 
+
+    # Q: Sample objective function to minimize (e.g. sample average of negative log-livelihood)
+    Q     = lambda theta:  Qfun(theta, out='Q')
+
+    # dQ: Derivative of sample objective function wrt parameters theta (function returns size K array)
+    dQ = None
+    if deriv>0: # use user-supplied 1 order derivatives
+        dQ = lambda theta:  Qfun(theta, out='dQ')
+
+    hess = None
+    if deriv>1: # use user-supplied 2 order derivatives
+        hess  =  lambda theta:  Qfun(theta, out='H')
+        res=minimize(fun=Q, jac=dQ, hess=hess, x0=theta0, method='trust-ncg')
+        res.hess_inv=la.inv(res.hess)
+    else: # use bfgs
+        res=minimize(fun=Q, jac=dQ, x0=theta0, method='bfgs')
+    
+    theta_hat=np.array(res.x).reshape(-1,1)
+    
+    toc = time.perf_counter()
+
+    # variance co-variance matrix
+    s_i=Qfun(theta_hat, out='s_i')
+
+    cov = avar(s_i, res.hess_inv, cov_type)
+    se = np.sqrt(np.diag(cov)).reshape(-1, 1)
+
+    # collect output
+    names =   ['parnames', 'theta_hat', 'se',  't-values',  'cov', 'Q', 'time', 's_i']
+    results = [parnames, theta_hat, se, theta_hat/se, cov, Q(theta_hat), toc - tic, s_i]
+
+    res.update(dict(zip(names, results)))
+
+    if output: 
+        if res.parnames: 
+            table=({k:res[k] for k in ['parnames', 'theta_hat', 'se', 't-values', 'jac']})
+        else: 
+            table=({k:res[k] for k in ['theta_hat', 'se', 't-values', 'jac']})
+        print(tabulate(table, headers="keys",floatfmt="10.5f"))
+        print('')
+        print(res.message)
+        print('Objective function:', res['Q'])
+        print ('Iteration info: %d iterations, %d evaluations of objective, and %d evaluations of gradients' 
+            % (res.nit,res.nfev, res.njev))
+        print(f"Elapsed time: {res['time']:0.4f} seconds")
+
+    return res
+
+def avar(s_i, Ainv, cov_type ='sandwich'):
+    n, K=s_i.shape
+    B=s_i.T@ s_i/n 
+    if cov_type=='Ainv':        return Ainv/n;         
+    if cov_type=='Binv':        return la.inv(B)/n
+    if cov_type=='sandwich':    return Ainv @ B @ Ainv/n
+
 
 def sim_data(N, J, theta) -> tuple:
     k = theta.size
@@ -29,7 +90,7 @@ def clogit(y, x, cov_type='Ainv',theta0=None, deriv=0, quiet=False):
     if theta0 is None: 
     	theta0=np.zeros((K,1)) 
 
-    res = M.estimation(Qfun, theta0, deriv, cov_type, parnames=xvars)
+    res = estimation(Qfun, theta0, deriv, cov_type, parnames=xvars)
     # v, p, dv = Qfun(res.theta_hat, out='predict')
     res.update(dict(zip(['yvar', 'xvars', 'N','K', 'n'], ['y', xvars, N, K, N])))
 
