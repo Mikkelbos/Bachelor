f9f400e651f3b90a955a4335b55fea9b3e78d30a
Mikkelbos <mikkeldollerup@gmail.com>
2024-03-06 13:04:39 +0100

    function logit()
    Alle funktioner virker nu

--- functions.py
+++ functions.py
@@ -1,17 +1,7 @@
+import statsmodels.api as sm
 import numpy as np
-import pandas as pd
-import mestim as M
-from tabulate import tabulate
 from numpy import linalg as la
-from scipy.optimize import minimize
-from numpy import random
-import time
-from scipy.stats import genextreme
-import mestim as M
-
-
-#def s_j(alpha, beta, x_j, p_j):
-#    return alpha*p_j + x_j*beta 
+import pandas as pd
 
 def exp_delta(alpha, beta, X, p_j):
     s_j = []
@@ -22,66 +12,84 @@ def exp_delta(alpha, beta, X, p_j):
         
     for i in range (len(s_j)):
         exp_s_j.append(np.exp(s_j[i]))
-    return np.array(exp_s_j)
+    return exp_s_j
 
-def ccp(alpha, beta, x_j, p_j):
+def ccp(alpha, beta, X, p_j):
     ccp_list = [] 
-    exp_delta_list = exp_delta(alpha, beta, x_j, p_j)
+    exp_delta_list = exp_delta(alpha, beta, X, p_j)
     sum_exp = np.sum(exp_delta_list)
     
     for i in range(len(exp_delta_list)):
         ccp_list.append(exp_delta_list[i]/sum_exp) 
-    print(np.sum(ccp_list))
-    return np.array(ccp_list)
+    print(f'choice probability sum: {np.sum(ccp_list)} \n 3 highest probability: {np.sort(ccp_list, axis=0)[-3:]}')
+    return ccp_list
 
-
-
-def probability_ratio(ccp, index, columns): #index = alternative j, columns = alternative i
-    probability_ratio_matrix = pd.DataFrame(index = index, columns = columns)
+def probability_ratio(ccp, model_labels, columns): #index = alternative j, columns = alternative i
+    probability_ratio_matrix = pd.DataFrame(index = model_labels, columns = columns)
     for i in range(len(ccp)):
         for j in range(len(ccp)):
             probability_ratio_matrix.iloc[i,j] = ccp[i]/ccp[j]
     #print(f'probability_ratio_matrix: \n{probability_ratio_matrix}')
     return probability_ratio_matrix
 
-def marginal_effects(ccp, index, columns, coefficients):
-    marginal_effects = pd.DataFrame(index = index, columns = columns)
+def marginal_effects(ccp, model_labels, coefficients_labels, coefficients):
+    marginal_effects = pd.DataFrame(index = model_labels, columns = coefficients_labels)
     for i in range(len(ccp)):
         for j in range(len(coefficients)-1):
             marginal_effects.iloc[i,j] = coefficients[j+1]*ccp[i]*(1-ccp[j])
     #print(f'marginal_effects: \n{marginal_effects}')
     return marginal_effects
 
-def cross_marginal_effects(ccp, index, columns, coefficients):
+def cross_marginal_effects(ccp, coefficients):
     cross_marginal_effects = np.zeros((len(ccp), len(ccp), len(coefficients)-1))
     for i in range(len(ccp)):
         for j in range(len(ccp)):
             for k in range(len(coefficients)-1):
                 cross_marginal_effects[i,j, k] = -coefficients[k+1]*ccp[i]*ccp[j]
+    #print(f'cross_marginal_effects: \n {cross_marginal_effects}')
 
     return cross_marginal_effects
 
-def elasticity (ccp, index, columns, coefficients, X):
-    elasticity = pd.DataFrame(index = index, columns = columns)
-    X = X[:,1:]
-    for i in range(len(ccp)):
+def elasticity(ccp, model_labels, coefficients_labels, coefficients, X):
+    elasticity = pd.DataFrame(index = model_labels, columns = coefficients_labels)
+    #print(elasticity)
+    X = X[:,1:].reshape(1, -1)
+  
+    for i in range(len(model_labels)):
         for j in range(len(coefficients)-1):
-            elasticity.iloc[i,j] = ((coefficients[j+1])*X[i]*(1 - ccp[i]))
-    print(elasticity)
+            elasticity.iloc[i,j] = ((coefficients[j+1])*X[:,j:j+1]*(1 - ccp[i]))
+    #print(elasticity)
     return elasticity
 
-def cross_elasticity(ccp, index, columns, coefficients, X):
+def print_cross_elasticity(cross_elasticity, model_labels):
+    print(cross_elasticity.shape[1])
+    for k in range(cross_elasticity.shape[1]):
+        print(f'Change in : {model_labels[k]} \n {cross_elasticity[:,k:k+1,:]}')
+
+def cross_elasticity(ccp, coefficients, X, model_labels):
     cross_elasticity = np.zeros((len(ccp), len(ccp), len(coefficients)-1))
     X = X[:,1:] #remove the constant
-    for i in range(len(ccp)):
-        for j in range(len(ccp)):
-            for k in range(len(coefficients)-1):
-                cross_elasticity[i,j, k] = -coefficients[k+1]*X[i]*(ccp[j])
-    print(cross_elasticity)
+    for k in range(len(coefficients)-1):
+        for i in range(len(ccp)):
+            for j in range(len(ccp)):
+                cross_elasticity[i,j, k] = -coefficients[k+1]*X[1,k]*(ccp[j])
+    #print_cross_elasticity(cross_elasticity, model_labels)
     return cross_elasticity
     
+
+def est_OLS(y, X, xnames):
+    model = sm.OLS(y, X)
+    results = model.fit()
+    print(results.summary(xname= xnames, yname='Market share'))
+    return results
    
    
-   
-   
-   
+def logit(alpha, beta, X, p_j, model_labels, coefficients_labels, coefficients):
+    ccp_list = ccp(alpha, beta, X, p_j)
+    probability_ratio_matrix = probability_ratio(ccp_list, model_labels, model_labels)
+    marginal_effects_matrix = marginal_effects(ccp_list, model_labels, coefficients_labels, coefficients)
+    cross_marginal_effects_matrix = cross_marginal_effects(ccp_list, coefficients)
+    elasticity_matrix = elasticity(ccp_list, model_labels, coefficients_labels, coefficients, X)
+    cross_elasticity_matrix = cross_elasticity(ccp_list, beta, X, model_labels)
+
+    #return ccp_list, probability_ratio_matrix#, marginal_effects_matrix, cross_marginal_effects_matrix, elasticity_matrix, cross_elasticity_matrix
