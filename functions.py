255208b1cfa2d66f1b151dd3d097696547360744
Valther7 <rjt675@alumni.ku.dk>
2024-05-12 22:46:04 +0200

    update

--- functions.py
+++ functions.py
@@ -252,20 +252,18 @@ def straf_0ms(df):
 
 #Cost
 
-def cost_original(dataset, estimation):
+def cost_original(dataset, alpha):
     s = dataset['CCP']
     p_j = dataset['Price']
-    alpha = estimation.params[1]
     cost = np.zeros(len(p_j))
     for i in range(len(p_j)):
         cost[i] = p_j[i] + (alpha/s[i])
         #Hvis cost stadig er højere end pris, så ændre "-" til "+" og køre igen
     return cost
 
-def cost(dataset, estimation):
+def cost(dataset, alpha):
     p_j = dataset['Price']
     ccp = dataset['CCP']
-    alpha = estimation.params[1]
 
     cost = np.zeros(len(p_j))
     for i in range(len(p_j)):
